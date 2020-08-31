b39e8cc3b1b5021bc1c69ddb191c616699074922
tianhuizhou <tianhui@hawaii.edu>
2020-08-31 02:12:40 -1000

    initialize the project
    connect it with firebase

---
+++ Bowfolios-swiftUI/Bowfolios-swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bowfolios-swiftUI
+//
+//  Created by Tianhui Zhou on 8/31/20.
+//  Copyright © 2020 Tianhui Zhou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
