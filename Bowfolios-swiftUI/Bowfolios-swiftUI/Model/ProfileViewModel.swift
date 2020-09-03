17364eb91292fbb43ace1f480119bae345ea2380
tianhuizhou <tianhui@hawaii.edu>
2020-09-02 15:10:02 -1000

    Fixed Bugs
    The last issue/bug is when I directly change the data in Firebase, it will cause duplication in the views since it automatically reload data from Firebase if it's changed.
    
    Solution:
    I modified and specify the condition for appending data to collections.
    And specify the changes.
    
    result:
    The collection still reload data if it's changed. But it can recognize updates or adding new data.
    when the view is reloaded, the new imformation will be dislplayed

--- Bowfolios-swiftUI/Bowfolios-swiftUI/Model/ProfileViewModel.swift
+++ Bowfolios-swiftUI/Bowfolios-swiftUI/Model/ProfileViewModel.swift
@@ -29,8 +29,28 @@ class ProfileViewModel: ObservableObject{
                 let profile = try! doc.document.data(as: Profile.self)
                 
                 if profile != nil {
-                    self.profiles.append(profile!)
+                   
                     
+                    if self.profiles.isEmpty {
+                        //print("should be display only once")
+                        self.profiles.append(profile!)
+                        //print("I am adding \(profile!.Name)")
+                    } else {
+                        var jg = 0
+                        for var eachProfile in self.profiles {
+                            if eachProfile.Name == profile!.Name{
+                                print("I am Changing \(profile!.Name)")
+                                eachProfile = profile!
+                                jg = 1
+                                break
+                            }
+                        }
+                        
+                        if jg == 0 {
+                            self.profiles.append(profile!)
+                            print("I am adding \(profile!.Name)")
+                        }
+                    }
 //                    for eachInterest in profile!.interests{
 //                        if !self.allInterests.contains(eachInterest) {
 //                            self.allInterests.append(eachInterest)
@@ -40,6 +60,7 @@ class ProfileViewModel: ObservableObject{
                 } else {
                     print("Error in loading database!")
                 }
+                print("the size of collection: \(self.profiles.count)")
             }
         }
         print("test the get files func")
