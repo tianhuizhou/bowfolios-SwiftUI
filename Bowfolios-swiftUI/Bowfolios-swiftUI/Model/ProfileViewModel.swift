5c2ef10c10748ac0163dbff47f05ba1d4d1b6943
tianhuizhou <tianhui@hawaii.edu>
2020-09-03 22:03:08 -1000

    fixed bugs
    using getDocument() method to retrive data from firebase once instead of adding snapshot listener(the way of getting real-time data)

--- Bowfolios-swiftUI/Bowfolios-swiftUI/Model/ProfileViewModel.swift
+++ Bowfolios-swiftUI/Bowfolios-swiftUI/Model/ProfileViewModel.swift
@@ -23,78 +23,73 @@ class ProfileViewModel: ObservableObject{
         
         //reset to default as empty collections to avoid duplication
         profiles = []
-        //interestsAndMembers = []
-        
-        ref.collection("Profiles").addSnapshotListener { (snap, error) in
-            
+        interestsAndMembers = []
+        ref.collection("Profiles").getDocuments { (snap, error) in
             guard let docs = snap else{return}
-            
-            docs.documentChanges.forEach { (doc) in
                 
-                let profile = try! doc.document.data(as: Profile.self)
-                
-                if profile != nil {
+                docs.documentChanges.forEach { (doc) in
                     
+                    let profile = try! doc.document.data(as: Profile.self)
                     
-                    if self.profiles.isEmpty {
-                        //print("should be display only once")
-                        self.profiles.append(profile!)
-                        //print("I am adding \(profile!.Name)")
+                    if profile != nil {
                         
-                    } else {
-                        var jg = 0
-                        for var eachProfile in self.profiles {
-                            if eachProfile.Name == profile!.Name{
-                                print("I am Changing \(profile!.Name)")
-                                eachProfile = profile!
-                                jg = 1
-                                break
+                        
+                        if self.profiles.isEmpty {
+                            //print("should be display only once")
+                            self.profiles.append(profile!)
+                            //print("I am adding \(profile!.Name)")
+                            
+                        } else {
+                            var jg = 0
+                            for var eachProfile in self.profiles {
+                                if eachProfile.Name == profile!.Name{
+                                    print("I am Changing \(profile!.Name)")
+                                    eachProfile = profile!
+                                    jg = 1
+                                    break
+                                }
+                            }
+                            
+                            if jg == 0 {
+                                self.profiles.append(profile!)
+                                print("I am adding \(profile!.Name)")
                             }
                         }
                         
-                        if jg == 0 {
-                            self.profiles.append(profile!)
-                            print("I am adding \(profile!.Name)")
+                        for eachIntes in profile!.interests {
+
+                            // } else {
+                            var numJg = 0
+                            if self.interestsAndMembers.count > 0 {
+                                for num in 0...(self.interestsAndMembers.count-1) {
+                                    if self.interestsAndMembers[num].theInterest == eachIntes {
+                                        numJg = 1
+                                        //ea.members.append(profile!.Name)
+                                        self.interestsAndMembers[num].members.append(profile!.Name)
+                                        print("only name")
+                                    }
+                                }
+                            }
+                            if numJg == 0 {
+                                print("New interest")
+                                self.interestsAndMembers.append(anInterest(theInterest: eachIntes, members: [profile!.Name]))
+                            }
+                            // }
+                            print(self.interestsAndMembers)
                         }
+                        
+                    } else {
+                        print("Error in loading database!")
                     }
-                    
-//                    for eachIntes in profile!.interests {
-//
-//                        // } else {
-//                        var numJg = 0
-//                        if self.interestsAndMembers.count > 0 {
-//                            for num in 0...(self.interestsAndMembers.count-1) {
-//                                if self.interestsAndMembers[num].theInterest == eachIntes {
-//                                    numJg = 1
-//                                    //ea.members.append(profile!.Name)
-//                                    self.interestsAndMembers[num].members.append(profile!.Name)
-//                                    print("only name")
-//                                }
-//                            }
-//                        }
-//                        if numJg == 0 {
-//                            print("New interest")
-//                            self.interestsAndMembers.append(anInterest(theInterest: eachIntes, members: [profile!.Name]))
-//                        }
-//                        // }
-//                        print(self.interestsAndMembers)
-//                    }
-                    
-                } else {
-                    print("Error in loading database!")
+                    print("the size of collection: \(self.profiles.count)")
                 }
-                print("the size of collection: \(self.profiles.count)")
+                
             }
-            
+            print("test the get files func")
         }
-        print("test the get files func")
         
     }
     
-    func groupInterteMembers() {
-        interestsAndMembers = []
-        
-    }
     
-}
+
 
